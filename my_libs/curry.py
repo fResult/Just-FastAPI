7698efa3956f1e3799b256bd25b1cd10d5422453
sila-strike <ssetthakananan@strikesocial.com>
2024-07-16 01:23:20 +0700

    update compose lib and example to be generic in python v3.12

--- my_libs/curry.py
+++ my_libs/curry.py
@@ -1,86 +1,75 @@
 from typing import Callable, TypeVar, overload
 from functools import reduce
 
-T = TypeVar("T")
-T1 = TypeVar("T1")
-T2 = TypeVar("T2")
-T3 = TypeVar("T3")
-T4 = TypeVar("T4")
-T5 = TypeVar("T5")
-T6 = TypeVar("T6")
-T7 = TypeVar("T7")
-R = TypeVar("R")
-
-
-def identity(x: T1) -> T1:
+def identity[T](x: T) -> T:
     return x
 
 
 @overload
-def compose(f1: Callable[[T1], T2]) -> Callable[[T1], T2]:
+def compose[T, R](f1: Callable[[T], R]) -> Callable[[T], R]:
     ...
 
 
 @overload
-def compose(
+def compose[T1, T2, R](
+    f2: Callable[[T2], R],
+    f1: Callable[[T1], T2],
+) -> Callable[[T1], R]:
+    ...
+
+
+@overload
+def compose[T1, T2, T3, R](
+    f3: Callable[[T3], R],
     f2: Callable[[T2], T3],
     f1: Callable[[T1], T2],
-) -> Callable[[T1], T3]:
+) -> Callable[[T1], R]:
     ...
 
 
 @overload
-def compose(
+def compose[T1, T2, T3, T4, R](
+    f4: Callable[[T4], R],
     f3: Callable[[T3], T4],
     f2: Callable[[T2], T3],
     f1: Callable[[T1], T2],
-) -> Callable[[T1], T4]:
+) -> Callable[[T1], R]:
     ...
 
 
 @overload
-def compose(
+def compose[T1, T2, T3, T4, T5, R](
+    f5: Callable[[T5], R],
     f4: Callable[[T4], T5],
     f3: Callable[[T3], T4],
     f2: Callable[[T2], T3],
     f1: Callable[[T1], T2],
-) -> Callable[[T1], T5]:
+) -> Callable[[T1], R]:
     ...
 
 
 @overload
-def compose(
+def compose[T1, T2, T3, T4, T5, T6, R](
+    f6: Callable[[T6], R],
     f5: Callable[[T5], T6],
     f4: Callable[[T4], T5],
     f3: Callable[[T3], T4],
     f2: Callable[[T2], T3],
     f1: Callable[[T1], T2],
-) -> Callable[[T1], T6]:
+) -> Callable[[T1], R]:
     ...
 
 
 @overload
-def compose(
+def compose[T1, T2, T3, T4, T5, T6, T7, R](
+    f7: Callable[[T7], R],
     f6: Callable[[T6], T7],
     f5: Callable[[T5], T6],
     f4: Callable[[T4], T5],
     f3: Callable[[T3], T4],
     f2: Callable[[T2], T3],
     f1: Callable[[T1], T2],
-) -> Callable[[T1], T7]:
-    ...
-
-
-@overload
-def compose(
-    f7: Callable[[R], T7],
-    f6: Callable[[T6], T7],
-    f5: Callable[[T5], T6],
-    f4: Callable[[T4], T5],
-    f3: Callable[[T3], T4],
-    f2: Callable[[T2], T3],
-    f1: Callable[[T1], T2],
-) -> Callable[[T1], T7]:
+) -> Callable[[T1], R]:
     ...
 
 
@@ -104,11 +93,11 @@ def is_odd(x: int) -> bool:
     return x % 2 != 0
 
 
-def mymap(f: Callable[[T1], T2]) -> Callable[[list[T1]], list[T2]]:
+def mymap[T, R](f: Callable[[T], R]) -> Callable[[list[T]], list[R]]:
     return lambda xs: [f(x) for x in xs]
 
 
-def myfilter(f: Callable[[T1], bool]) -> Callable[[list[T1]], list[T1]]:
+def myfilter[T](f: Callable[[T], bool]) -> Callable[[list[T]], list[T]]:
     return lambda xs: [x for x in xs if f(x)]
 
 
@@ -118,3 +107,4 @@ twice_only_if_odd = compose(mymap(twice), myfilter(is_odd))
 times_4(8)  # 32
 twice_only_if_odd([1, 2, 3, 4, 5, 6, 7, 8, 9])  # [2, 6, 10, 14, 18]
 mymap(compose(sqr, twice))([1, 2, 3, 4, 5, 6, 7, 8, 9]) # [4, 16, 36, 64, 100, 144, 196, 256, 324]
+print(times_4(8))
